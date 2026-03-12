36d60f81f949d30bb6fe818fe8822808728d57b4
dong99u <qkrehdrb0813@gmail.com>
2026-03-12 20:36:31 +0900

    ㅇㅇ

--- algorithm/codetree/dp.py
+++ algorithm/codetree/dp.py
@@ -1,15 +1,17 @@
 n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [-1] * (n + 1)
-dp[0] = 1
-dp[1] = 1
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = grid[0][0]
 
-# 노드의 총 개수
-for i in range(2, n + 1):
-    sum_val = 0
-    # j = 루트 노드의 값
-    for j in range(1, i + 1):
-        sum_val += dp[j - 1] * dp[i - j]
-    dp[i] = sum_val
+for i in range(1, n):
+    dp[i][0] = dp[i - 1][0] + grid[i][0]
 
-print(dp[n])
+for j in range(1, n):
+    dp[0][j] = dp[0][j - 1] + grid[0][j]
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+
+print(dp[n - 1][n - 1])
