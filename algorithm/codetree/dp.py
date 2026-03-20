e70b9bfdcdb183d84990bd64e20dd3e0d7c7f7ec
dong99u <qkrehdrb0813@gmail.com>
2026-03-20 15:38:01 +0900

    ✨ feat(dp): 부분집합 차이 최소화 알고리즘 추가
    백트래킹과 메모이제이션을 활용하여 주어진 배열의 부분집합 차이를 최소화하는 알고리즘 구현.

--- algorithm/codetree/dp.py
+++ algorithm/codetree/dp.py
@@ -1,15 +1,34 @@
 import sys
+
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-w, v = zip(*[tuple(map(int, input().split())) for _ in range(N)])
-w, v = list(w), list(v)
+INF = float('inf')
 
-dp = [0] * (M + 1)
+n = int(input())
+arr = list(map(int, input().split()))
+total_sum = sum(arr)
 
-for i in range(N):
-    for j in range(M + 1):
-        if w[i] <= j:
-            dp[j] = max(dp[j], dp[j - w[i]] + v[i])
+memo = {}
 
-print(dp[M])
+
+def backtrack(idx, acc):
+    state = (idx, acc)
+    B = total_sum - acc
+    if state in memo:
+        return memo[state]
+    if idx == n:
+        if acc == 0 or acc == total_sum:
+            return INF
+        return abs(acc - B)
+
+    result = INF
+    result = min(result, min(backtrack(idx + 1, acc + arr[idx]), backtrack(idx + 1, acc)))
+
+    memo[state] = result
+
+    return result
+
+
+answer = backtrack(0, 0)
+
+print(answer)
