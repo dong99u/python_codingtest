e40b088aa1cc16947c04f4efc25d2af8dd9951bd
dong99u <qkrehdrb0813@gmail.com>
2026-04-11 10:00:04 +0900

    .

--- algorithm/codetree/dp.py
+++ algorithm/codetree/dp.py
@@ -1,26 +1,21 @@
-import sys
+import sys; input = lambda: sys.stdin.readline().rstrip()
 
-input = sys.stdin.readline
-
-INF = float('inf')
+INF = sys.maxsize
 
 n = int(input())
-arr = list(map(int, input().split()))
-total_sum = sum(arr)
+arr = [0] + list(map(int, input().split()))
 
-memo = {}
+dp = [[-INF] * 4 for _ in range(n + 1)]
+dp[0][0] = 0
+dp[1][1] = arr[1]
 
+for i in range(2, n + 1):
+    # 두 칸 전에서 점프
+    for j in range(4):
+        dp[i][j] = max(dp[i][j], dp[i - 2][j] + arr[i])
 
-dp = [0] * (M + 1)
+    # 한 칸 전에서 점프
+    for j in range(1, 4):
+        dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + arr[i])
 
-    result = INF
-    result = min(result, min(backtrack(idx + 1, acc + arr[idx]), backtrack(idx + 1, acc)))
-
-    memo[state] = result
-
-    return result
-
-
-answer = backtrack(0, 0)
-
-print(answer)
+print(max(dp[n]))
